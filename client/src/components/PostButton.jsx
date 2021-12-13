0e6ca56b283a78e82d3d2ad9cd6341b132fbf863
Juicestus <jus@gtsbr.org>
2021-12-13 17:23:53 -0600

    post popup pretty much

--- client/src/components/PostButton.jsx
+++ client/src/components/PostButton.jsx
@@ -1,36 +1,31 @@
 import React, { Component } from "react";
 
-import PostPopup from './PostPopup.jsx';
+import PostPopup from "./PostPopup.jsx";
 
 class PostButton extends Component {
-
   handleSubmit = (e) => {
     e.preventDefault();
-    console.log('You clicked submit.');
-  }
+    console.log("You clicked submit.");
+  };
 
   state = { seen: false };
 
   togglePop = () => {
     this.setState({
-      seen: !this.state.seen
+      seen: !this.state.seen,
     });
   };
 
-  /*return (
-    <form onSubmit={handleSubmit}>
-      <button className="solid-button" type="submit">
-        New Post
-      </button>
-    </form>
-  );*/
-
-  render() { return (
-    <div>
-      <a className="cool-button" onClick={this.togglePop} href="#">New Post</a>
-      {this.state.seen ? <PostPopup toggle={this.togglePop} /> : null}
-    </div>
-  );}
+  render() {
+    return (
+      <div>
+        <a className="transparent-button open-new-post-window" onClick={this.togglePop}>
+          {!this.state.seen ? "New Post" : "Cancel"}
+        </a>
+        {this.state.seen ? <PostPopup toggle={this.togglePop} /> : null}
+      </div>
+    );
+  }
 }
-  
+
 export default PostButton;
--- client/src/components/PostPopup.jsx
+++ client/src/components/PostPopup.jsx
@@ -1,18 +1,38 @@
 import React, { Component } from "react";
 
 class PostPopup extends Component {
-    handleClick = () => {
-        this.props.toggle();
-    };
+  state = { seen: false, message: "" };
+
+  exit = () => this.props.toggle();
+  
+  createPost = (e) => {
+    e.preventDefault();
     
-    render() { return (
-     <div className="modal">
-       <div className="modal_content">
-       <span className="close" onClick={this.handleClick}>&times;    </span>
-       <p>I'm A Pop Up!!!</p>
+    // Post the message!
+    console.log(this.state.message);
+
+    // Todo: charater count
+    this.exit();
+  };
+
+  render() {
+    return (
+      <div className="popup-struct">
+        <div className="popup-content">
+          <span className="popup-close" onClick={this.exit}>&times;{" "}</span>
+          <h2>Create a Post</h2>
+          <form id="post-msg-form" onSubmit={this.createPost}>
+            <textarea onChange={e => this.setState({message: e.target.value}) }
+              className="post-text"
+              placeholder="What's on your mind?"
+              value={this.state.message}
+            ></textarea>
+              <a className="transparent-button" onClick={this.createPost}>Create Post</a>
+          </form>
+        </div>
       </div>
-     </div>
-    );}
+    );
+  }
 }
 
 export default PostPopup;
